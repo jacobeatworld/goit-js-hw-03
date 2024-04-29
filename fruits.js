6c938a9cb94fc22f16f1ae62c5bb477feecd3d07
jacobeatworld <156788216+jacobeatworld@users.noreply.github.com>
2024-04-29 20:54:37 +0200

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "Nowy nagłówek";
+}
+
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
+
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+
