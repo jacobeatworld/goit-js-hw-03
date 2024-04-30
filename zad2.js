816ed352668bf1e3e253c25358a02bd73f061207
jacobeatworld <156788216+jacobeatworld@users.noreply.github.com>
2024-04-30 18:16:54 +0200

    Add files via upload

---
+++ zad2.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "Nowy nagłówek";
+}
+
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const wyznacznik = 5;
+for (let i=0; i <numbers.length; i += 1) {
+    if (numbers[i] > wyznacznik) {
+        console.log(numbers[i]);
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+
